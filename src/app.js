f83a608b95bee04522ca78cc8085004a491b8505
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-05-08 19:26:49 +0000

    generar-excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomwho = who[Math.floor(who.length * Math.random())];
+  let randomAction = action[Math.floor(action.length * Math.random())];
+  let randomWhat = what[Math.floor(what.length * Math.random())];
+  let randomWhen = when[Math.floor(when.length * Math.random())];
+
+  const excuseMessage = `${randomwho} ${randomAction} ${randomWhat} ${randomWhen} `;
+
+  excuse.innerHTML = excuseMessage;
 };
